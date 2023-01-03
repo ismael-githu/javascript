29f1c06c4f6e9828514b2c8bc69b2cd2c3258839
ismael-githu <114022731+ismael-githu@users.noreply.github.com>
2023-01-02 22:41:37 -0300

    atualização exercicios

---
+++ Exercicios/script.js
@@ -0,0 +1,11 @@
+function mostraData()
+{
+        document.getElementById("data").innerHTML=Date();
+}
+
+function mostraMensagem()
+{
+        alert('Estou aprendendo a programar em JavaScript!');
+}
+
+ 
